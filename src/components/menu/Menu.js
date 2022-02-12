c6e612842b0464ceb27373d6f7984bcc0c7e793b
ANDREYDEN <andrey2850@gmail.com>
2022-02-12 18:11:53 -0500

    Clean up menu logo

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,14 +1,9 @@
 import { Link } from "react-router-dom";
 import "./Menu.css";
-import logo from "../../assets/images/logo_GDSC.png";
 
 function Menu() {
   return (
     <div className="menu">
-      {/* <Link className="menu-header" to="/">
-        <img src={logo} alt="gdsc logo"></img>
-        <span>GDSC</span>
-      </Link> */}
       <Link className="menu-item" to="/">
         Home
       </Link>
