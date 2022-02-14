623d2fc33aff7ace91225d0494f4976aa5d1dc7f
ANDREYDEN <andrey2850@gmail.com>
2022-02-14 18:31:55 -0500

    Add basic event list

--- src/pages/calendar/Calendar.js
+++ src/pages/calendar/Calendar.js
@@ -1,9 +1,44 @@
-import ComingSoon from '../../components/ComingSoon/ComingSoon';
+import './Calendar.css'
+
+function renderEvent(event) {
+  return (
+    <div key={event.id} className='event'>
+      <h2>{event.title}</h2>
+      <p>{event.description}</p>
+      <p>{event.startTime.toDateString()}</p>
+      <p>{event.duration}h</p>
+    </div>
+  )
+}
 
 function Calendar() {
+  const events = [
+    {
+      id: "1",
+      title: "Game Night",
+      description: "Setting up the calendar page",
+      startTime: new Date(),
+      duration: 1.0
+    },
+    {
+      id: "2",
+      title: "Info session",
+      description: "Setting up the calendar page",
+      startTime: new Date(),
+      duration: 1.0
+    },
+    {
+      id: "3",
+      title: "Coding Mondays #3",
+      description: "Setting up the calendar page",
+      startTime: new Date(),
+      duration: 1.0
+    }
+  ]
   return (
-    <div>
-      <ComingSoon/>
+    <div className="calendar-container">
+      <h1>Upcoming Events</h1>
+      {events.map(renderEvent)}
     </div>
   );
 }
