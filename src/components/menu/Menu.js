ecd156b97351f4194eac74a2e24ef6f63587a4f2
Amit Kumar <ellipticamit@gmail.com>
2022-02-18 09:02:06 +0530

    header: Hamburge, active link underline and logo added

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,21 +1,38 @@
-import { Link } from "react-router-dom";
-import "./Menu.css";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import './Menu.css';
 
 function Menu() {
+  const [toggle, setToggle] = useState(false);
+  const location = useLocation();
+  const { pathname } = location;
+
   return (
-    <div className="menu">
-      <Link className="menu-item" to="/">
-        Home
-      </Link>
-      <Link className="menu-item" to="/resources">
-        Resources
-      </Link>
-      <Link className="menu-item" to="/calendar">
-        Calendar
-      </Link>
-      <Link className="menu-item" to="/blog">
-        Blog
-      </Link>
+    <div className={`menu ${toggle ? 'activehamburger' : ''}`}>
+      <div className='logo'>
+        <img src='./logo_GDSC.png' alt='GDSC' />
+      </div>
+      <ul className='nav'>
+        <li className={`menu-item ${pathname === '/' ? 'active' : ''}`}>
+          <Link to='/'>Home</Link>
+        </li>
+        <li
+          className={`menu-item ${pathname === '/resources' ? 'active' : ''}`}>
+          <Link to='/resources'>Resources</Link>
+        </li>
+        <li className={`menu-item ${pathname === '/calendar' ? 'active' : ''}`}>
+          <Link to='/calendar'>Calendar</Link>
+        </li>
+        <li className={`menu-item ${pathname === '/blog' ? 'active' : ''}`}>
+          <Link to='/blog'>Blog</Link>
+        </li>
+      </ul>
+      <div className='hamburger' onClick={() => setToggle(!toggle)}>
+        <span className='line line_1'></span>
+        <span className='line line_2'></span>
+        <span className='line line_3'></span>
+      </div>
     </div>
   );
 }
