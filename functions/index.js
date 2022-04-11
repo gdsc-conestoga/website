4754b9ff48f39d7e107b66d35090961280882545
ANDREYDEN <andrey2850@gmail.com>
2022-04-11 18:54:53 -0400

    Change function trigger

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,21 @@ const admin = require("firebase-admin")
 
 admin.initializeApp()
 
-exports.makeAdmin = functions.https.onRequest((request, response) => {
-  const { uid } = request.query
-  admin.auth().setCustomUserClaims(uid, { 'admin': true })
-  response.sendStatus(200);
+exports.makeAdmin = functions.firestore
+    .document('admins/{uid}')
+    .onWrite(async (change, context) => {
+  const docCreated = !change.before.exists
+  const docDeleted = !change.after.exists
+  const { uid } = context.params
+  try {
+    if (docCreated || docDeleted) {
+      await admin.auth().setCustomUserClaims(uid, { 'admin': docCreated })
+      functions.logger.log(`User ${uid} is now ${docCreated ? 'an admin' : 'a regular user'}`)
+    }
+  } catch (e) {
+    functions.logger.log(`Something went wrong while updating an admin (${uid}): ${e.message}`, e)
+  } finally {
+    return null
+  }
 });
+
